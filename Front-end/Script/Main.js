bd3a2a332965708bf3dd18900e96e7e95f7f7263
Your Name <you@your-domain.com>
2025-11-10 14:57:30 -0300

    Updated

--- Front-end/Script/Main.js
+++ Front-end/Script/Main.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000"; // Ajuste conforme o backend
+const API_BASE = "https://yourcontrol.onrender.com"; // Ajuste conforme o backend
 
 // === Alternar telas ===
 function mostrarTela(nome) {
